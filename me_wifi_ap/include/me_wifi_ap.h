8488c1d644248fc5df4489bdddccf63dd1b6e605
Maxim Amzarakov <maxam18@gmail.com>
2022-01-17 23:26:31 +0300

    wifi AP added

---
+++ me_wifi_ap/include/me_wifi_ap.h
@@ -0,0 +1,14 @@
+/* My ESP wifi ap. From example.
+ * File: me_wifi_ap.h
+ * Started: Wed Nov 10 22:04:05 MSK 2021
+ * Author: Max Amzarakov (maxam18 _at_ gmail _._ com)
+ * Copyright (c) 2021 ..."
+ */
+
+#ifndef _ME_WIFI_AP_H
+#define _ME_WIFI_AP_H
+
+void me_wifi_ap_init();
+
+#endif
+
---
+++ me_wifi_ap/me_wifi_ap.c
@@ -0,0 +1,76 @@
+/* My ESP wifi ap. From example.
+ * File: me_wifi_ap.c
+ * Started: Wed Nov 10 22:03:58 MSK 2021
+ * Author: Max Amzarakov (maxam18 _at_ gmail _._ com)
+ * Copyright (c) 2021 ..."
+ */
+
+#include <me_wifi_ap.h>
+
+#include <string.h>
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
+#include "esp_system.h"
+#include "esp_wifi.h"
+#include "esp_event.h"
+#include "esp_log.h"
+
+#include "lwip/err.h"
+#include "lwip/sys.h"
+
+/* The examples use WiFi configuration that you can set via project configuration menu.
+
+   If you'd rather not, just change the below entries to strings with
+   the config you want - ie #define EXAMPLE_WIFI_SSID "mywifissid"
+*/
+
+static const char *TAG = "me wifi AP";
+
+static void wifi_event_handler(void* arg, esp_event_base_t event_base,
+                                    int32_t event_id, void* event_data)
+{
+    if (event_id == WIFI_EVENT_AP_STACONNECTED) {
+        wifi_event_ap_staconnected_t* event = (wifi_event_ap_staconnected_t*) event_data;
+        ESP_LOGI(TAG, "station "MACSTR" join, AID=%d",
+                 MAC2STR(event->mac), event->aid);
+    } else if (event_id == WIFI_EVENT_AP_STADISCONNECTED) {
+        wifi_event_ap_stadisconnected_t* event = (wifi_event_ap_stadisconnected_t*) event_data;
+        ESP_LOGI(TAG, "station "MACSTR" leave, AID=%d",
+                 MAC2STR(event->mac), event->aid);
+    }
+}
+
+void me_wifi_ap_init(void)
+{
+    esp_netif_create_default_wifi_ap();
+
+    wifi_init_config_t cfg = WIFI_INIT_CONFIG_DEFAULT();
+    ESP_ERROR_CHECK(esp_wifi_init(&cfg));
+
+    ESP_ERROR_CHECK(esp_event_handler_instance_register(WIFI_EVENT,
+                                                        ESP_EVENT_ANY_ID,
+                                                        &wifi_event_handler,
+                                                        NULL,
+                                                        NULL));
+
+    wifi_config_t wifi_config = {
+        .ap = {
+            .ssid = CONFIG_ME_WIFI_SSID,
+            .ssid_len = strlen(CONFIG_ME_WIFI_SSID),
+            .channel = CONFIG_ME_WIFI_CHANNEL,
+            .password = CONFIG_ME_WIFI_PASSWORD,
+            .max_connection = CONFIG_ME_WIFI_AP_MAX,
+            .authmode = WIFI_AUTH_WPA_WPA2_PSK
+        },
+    };
+    if (strlen(CONFIG_ME_WIFI_SSID) == 0) {
+        wifi_config.ap.authmode = WIFI_AUTH_OPEN;
+    }
+
+    ESP_ERROR_CHECK(esp_wifi_set_mode(WIFI_MODE_AP));
+    ESP_ERROR_CHECK(esp_wifi_set_config(ESP_IF_WIFI_AP, &wifi_config));
+    ESP_ERROR_CHECK(esp_wifi_start());
+
+    ESP_LOGI(TAG, "wifi_init_softap finished. SSID:%s password:%s channel:%d",
+             CONFIG_ME_WIFI_SSID, CONFIG_ME_WIFI_PASSWORD, CONFIG_ME_WIFI_CHANNEL);
+}
