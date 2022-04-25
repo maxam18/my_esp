afe2c64cbdb29fe9753f11fc913b07780a3771c1
Maxim Amzarakov <maxam18@gmail.com>
2022-04-25 21:32:45 +0300

    signed integer in the loop

--- me_ncoder/me_ncoder.c
+++ me_ncoder/me_ncoder.c
@@ -117,7 +117,6 @@ static void call_encoder()
 
 esp_err_t me_ncoder_init(uint8_t *pins, uint8_t len, uint8_t pina, uint8_t pinb, xQueueHandle *qptr)
 {
-    uint8_t i;
     gpio_config_t   io_conf = {
         .mode       = GPIO_MODE_INPUT,
         .intr_type  = GPIO_INTR_DISABLE,
@@ -166,7 +165,8 @@ esp_err_t me_ncoder_init(uint8_t *pins, uint8_t len, uint8_t pina, uint8_t pinb,
         return ESP_ERR_INVALID_ARG;
 
 #ifdef CONFIG_ME_DEBUG
-    i = 0;
+    uint8_t i = 0;
+
     for( i = 0; i < enc->len; ++i ) 
     {
         me_debug("NCODER", "Button pin[%d] %d level %d", i, enc->pins[i]
